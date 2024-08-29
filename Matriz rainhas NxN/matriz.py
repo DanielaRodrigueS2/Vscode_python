a0d339e663476f89425d5320064880d2c3e2d719
Daniela <danielarodrigues2po@gmail.com>
2024-08-29 10:36:01 -0300

    Código sequencial completo

--- Matriz rainhas NxN/matriz.py
+++ Matriz rainhas NxN/matriz.py
@@ -17,13 +17,13 @@ def imprimeMatriz(matriz, tam):
 
 #função que testa se existem 2 rainhas em uma mesma coluna
 
-def testaColuna(matriz, tam, i, j):
+def testaColuna(matriz, tam, j):
     sum = 0
     for x in range(tam):
         if matriz[x][j] == 1:
             sum += 1
 
-    if sum < 2:
+    if sum == 0:
         return True
     else:
         return False
@@ -32,27 +32,28 @@ def testaColuna(matriz, tam, i, j):
 
 def testaDiagonalD(matriz, tam, i, j):
     sum = 0
-    x = i
-    y = j
+    x = i-1
+    y = j-1
 
-    #testa diagona de cima i+1 j+1
+    #testa diagona de cima i-1 j-1
     while(x >= 0 and y>=0):
+        if(matriz[x][y] == 1):
+            sum+= 1
         x -= 1
         y -=1
+        
+
+    x = i+1
+    y = j+1
+
+    #testa diagonal de baixo i+1 j+1
+    while(x < tam and y < tam):
         if(matriz[x][y] == 1):
             sum+= 1
-
-    x = i
-    y = j
-
-    #testa diagonal de baixo i-1
-    while(x < tam and y < tam):
         x += 1
         y +=1
-        if(matriz[x][y] == 1):
-            sum+= 1
 
-    if sum < 2:
+    if sum < 1:
         return True
     else:
         return False
@@ -62,41 +63,59 @@ def testaDiagonalD(matriz, tam, i, j):
    
 def testaDiagonaE(matriz, tam, i, j):
     sum = 0
-    x = i
-    y = j
+    x = i -1
+    y = j + 1
 
     # testa se existe outra rainha na diagonal esquerda acima i-1 j+1
     while(x>=0 and y < tam):
-        x -=1
-        y += 1
         if(matriz[x][y] == 1):
             sum+= 1
+        x -=1
+        y += 1
+        
 
-    x = i
-    y = j
+    x = i +1
+    y = j -1
 
     # testa se existe outra rainha na diagonal esquerda abaixo i+1 j-1
     while(x < tam and y >= 0):
-        x += 1
-        y -= 1
         if(matriz[x][y] == 1):
             sum+= 1
+        x += 1
+        y -= 1
+        
     
-    if sum < 2:
+    if sum < 1:
         return True
     else:
         return False
+    
 
-def colocaRainha(matriz, tam):
-    return
+# Função que testa todas as posições validas com as funções de teste de condição    
+def posicaoValida(matriz, i, j, tam):
+    return (testaColuna(matriz,tam, j) and testaDiagonalD(matriz, tam, i, j) and testaDiagonaE(matriz,tam, i, j))
+
+
+#Função para colocar as rainhas nas posições usando recursividade
+def colocaRainha(matriz, tam, i=0):
+    if i >= tam:
+        return True
+    
+    for j in range(tam):
+        if posicaoValida(matriz,i, j, tam):
+            matriz[i][j] = 1
+            if colocaRainha(matriz, tam, i+1):
+                return True
+            matriz[i][j] = 0
+
+    
     
     
 
 def main():
     tamanho = int(input("Digite o tamanho da matriz de NxN rainhas: "))
     matriz = criaMatriz(tamanho)
+    colocaRainha(matriz, tamanho)
     imprimeMatriz(matriz,tamanho)
-
-
 
 main()
